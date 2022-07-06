8fe3f9a962b6a6bab068d6197ee32364d86a66ec
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2022-07-06 18:37:42 +0700

    Wrap get database API

--- client/notion-client.js
+++ client/notion-client.js
@@ -5,13 +5,13 @@ export async function search(params) {
   const notion = new Client({ auth: process.env.NOTION_API_KEY });
   const response = await notion.search({
     query: params.query,
-    sort: {
-      direction: 'ascending',
-      timestamp: 'last_edited_time',
-    },
   });
-  console.log(response);
   return response
 }
 
-await search({ query: "task" });
+export async function getDatabase(databaseName) {
+  const databases = await search({ query: databaseName });
+  return databases.results.filter((result) => result.object == "database")
+}
+
+console.log(await getDatabase("projects"))
