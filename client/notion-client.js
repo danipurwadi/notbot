d7633b81ad9753588fc4f1f4d9c9e7259067feb3
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2022-07-06 18:42:42 +0700

    Wrap get all databases api

--- client/notion-client.js
+++ client/notion-client.js
@@ -3,15 +3,19 @@ import { Client } from "@notionhq/client";
 
 export async function search(params) {
   const notion = new Client({ auth: process.env.NOTION_API_KEY });
-  const response = await notion.search({
-    query: params.query,
-  });
+  const response = await notion.search(params);
   return response
 }
 
-export async function getDatabase(databaseName) {
-  const databases = await search({ query: databaseName });
-  return databases.results.filter((result) => result.object == "database")
+export async function getAllDatabases() {
+  const response = await search(null);
+  return response.results.filter((result) => result.object == "database")
 }
 
-console.log(await getDatabase("projects"))
+export async function getDatabase(databaseName) {
+  const response = await search({ query: databaseName });
+  return response.results.filter((result) => result.object == "database")
+}
+
+// console.log(await getDatabase("projects"))
+console.log(await getAllDatabases())
