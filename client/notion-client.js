6201eae51ef7c077208a87852244ce1c5a188a05
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2022-07-07 10:36:36 +0700

    Add get tasks due today feature

--- client/notion-client.js
+++ client/notion-client.js
@@ -29,3 +29,32 @@ export async function createEntry(databaseID, icon, properties, blocks) {
   });
   return response
 }
+
+export async function getDatabasePages(databaseID, filter, sorts) {
+  const notion = new Client({ auth: process.env.NOTION_API_KEY });
+  const response = await notion.databases.query({
+    database_id: databaseID,
+    filter: filter,
+    sorts: sorts,
+  });
+  return response
+}
+
+export async function getTasksDueToday() {
+  const tasksDatabase = await getDatabase("Tasks");
+  const databaseID = tasksDatabase[0].id;
+  const filter = {
+    property: "Deadline",
+    date: {
+      on_or_before: new Date().toISOString()
+    }
+  }
+  const sorts = [
+    {
+      property: 'Urgency',
+      direction: 'descending',
+    },
+  ]
+  const response = await getDatabasePages(databaseID, filter, sorts);
+  return response.results.filter((result) => result.object == "page" && result.properties.Status.status.name != "Done")
+}
